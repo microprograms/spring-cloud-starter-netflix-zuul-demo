bf5087b3939ef48c7a2fb83a3c1f3612b79b198c
xuzewei <microprograms@126.com>
2021-03-02 10:19:26 +0800

    spring-cloud-starter-netflix-zuul demo

---
+++ src/test/java/com/github/microprograms/spring_cloud_starter_netflix_zuul_demo/SpringCloudStarterNetflixZuulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.microprograms.spring_cloud_starter_netflix_zuul_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStarterNetflixZuulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
